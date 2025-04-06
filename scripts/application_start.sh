f2d49eec548b1a9858beaf8a166e0b54192b59eb
mohitksingh19 <mohit.k.singh@globallogic.com>
2025-04-06 10:29:13 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,9 @@ APP_NAME="Skycast"
 echo 'run application_start.sh: ' >> /home/ec2-user/Skycast/deploy.log
 echo 'pm2 restart Skycast' >> /home/ec2-user/Skycast/deploy.log
 if pm2 list | grep -qw "$APP_NAME"; then
-  pm2 restart 'Skycast' >> /home/ec2-user/Skycast/deploy1.log
+  echo "$APP_NAME is ReStarting the application..."
+  pm2 restart "$APP_NAME" >> /home/ec2-user/Skycast/deploy1.log
 else
-  pm2 start npm --name=Skycast -- start
+  echo "$APP_NAME is not registered. Starting the application..."
+  pm2 start ../app.js --name "$APP_NAME"
 fi
