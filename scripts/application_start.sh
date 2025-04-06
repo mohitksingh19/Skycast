01fa1e93e15473ba9336e9db9e730971ed732c9c
mohitksingh19 <mohit.k.singh@globallogic.com>
2025-04-06 11:25:57 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,5 +9,5 @@ if pm2 list | grep -qw "$APP_NAME"; then
   pm2 restart "$APP_NAME" >> /home/ec2-user/Skycast/deploy1.log
 else
   echo "$APP_NAME is not registered. Starting the application..."
-  pm2 start ../app.js --name "$APP_NAME"
+  pm2 start npm --name "$APP_NAME" -- start
 fi
